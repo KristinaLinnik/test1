79d7b5c44c0a3a48b17345443b816b96a1bd0722
Christina <stylistkris@gmail.com>
2017-06-19 21:50:49 +0200

    js

--- build/js/main.js
+++ build/js/main.js
@@ -24,33 +24,14 @@ $(document).ready(function () {
     var maxCount = 1000;
 
 
-    var angle = 0;
-    var direction = 'right';
+    var angle = -35;
     var div = document.getElementById('div');
-    setInterval(function() {
-        if (direction == 'right') {
-            angle++;
-            if (angle === 100) {
-                direction = 'left';
-            }
-        } else {
-            angle--;
-            if (angle === 0) {
-                direction = 'right';
-            }
-        }
-        div.style.webkitTransform = 'rotate(' + angle + 'deg)';
-        div.style.mozTransform = 'rotate(' + angle + 'deg)';
-        div.style.msTransform = 'rotate(' + angle + 'deg)';
-        div.style.oTransform = 'rotate(' + angle + 'deg)';
-        div.style.transform = 'rotate(' + angle + 'deg)';
-        // counterContainer.text(Math.round(angle/100*100));
-    }, 30);
-
     function countNumberInterval() {
         var i = 900;
         var timerId = setInterval(function() {
             counterContainer.text(i);
+            angle +=2;
+            div.style.transform = 'rotate(' + angle + 'deg)';
             if (i == maxCount) clearInterval(timerId);
             i++;
 
@@ -61,32 +42,3 @@ $(document).ready(function () {
 
 
 });
-
-//
-// $(document).ready(function () {
-//     var angle = 0;
-//     var direction = 'right';
-//     var div = $('#div');
-//     var mark = $('#mark');
-//     setInterval(function() {
-//         if (direction == 'right') {
-//             angle++;
-//             if (angle === 180) {
-//                 direction = 'left';
-//             }
-//         } else {
-//             angle--;
-//             if (angle === 0) {
-//                 direction = 'right';
-//             }
-//             }
-//             // div.style.webkitTransform = 'rotate(' + angle + 'deg)';
-//             // div.style.mozTransform = 'rotate(' + angle + 'deg)';
-//             // div.style.msTransform = 'rotate(' + angle + 'deg)';
-//             // div.style.oTransform = 'rotate(' + angle + 'deg)';
-//             div.style.transform = 'rotate(' + angle + 'deg)';
-//             mark.text(Math.round(angle / 180 * 100));
-//         },
-//         30);
-//
-// });
